c0db49fd06e467ee7b34de1e9138bb54e8e7808d
Fikri <jamesfikrii@gmail.com>
2019-04-24 09:04:16 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/navbar'
+import Home from './components/home'
+import Soal1 from './components/latihan1'
+import Soal3 from './components/latihan3'
+import { Route } from 'react-router-dom'
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Navbar/> 
+       <div className='container'>
+       <Route path='/' exact component={Home} />
+       <Route path='/latihan1' component={Soal1} />
+       <Route path='/petunjuk' component={Soal3} />
+       </div>
     </div>
   );
+  }
 }
 
 export default App;
